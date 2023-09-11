77398f34465dc60dbd522b2d9fd0aa6d408a1b08
Ragnhild Laursen <ragnhild@math.au.dk>
2023-09-11 11:24:00 -0700

    adding correlation with eigen

--- GaussianPrior/spatialGP.R
+++ GaussianPrior/spatialGP.R
@@ -2,9 +2,7 @@
 library(MASS)
 library(ggplot2)
 #locations 
-n = 1000 # datapoints
-#sx = c(runif(n/8,0,0.05),runif(n/8,0.6,1),runif(n/2,0.6,1),runif(n/4,0,0.4))
-#sy = c(runif(n/8,0,0.05),runif(n/8,0.6,1),runif(n/2,0,0.4),runif(n/4,0.6,1))
+n = 500 # datapoints
 
 # sampling x and y
 sx = runif(n)
@@ -12,16 +10,6 @@ sy = runif(n)
 
 s = cbind(sy,sx)
 dist = sapply(1:n,function(x) sapply(1:n,function(y) sqrt(sum((s[y,]-s[x,])^2))))
-
-# creating correlation matrix
-phi = 30
-
-#range
-range = 0.03
-phi = 3/range
-
-sigma = exp(-phi*dist)
-
 
 ## simulating some data
 # possible y
@@ -31,27 +19,54 @@ y = rep(1,n)
 idx = which(sx < 0.75 & sx > 0.25 & sy >0.25 & sy < 0.75)
 
 
-y[idx] = 2
+y[idx[1:100]] = 3
 
-yobs = y + rnorm(n,0,0.5)
+# putting a little normal noise on.
+yobs = y + rnorm(n,0,0.2)
+#yobs = y
+
+#weighting 
+range = 0.1
+phi = 3/range
+
+sigma = exp(-phi*dist)
 
 sigmanorm = diag(1/rowSums(sigma))%*%sigma
-znew = c(sigmanorm%*%yobs)
+weight = c(sigmanorm%*%yobs)
 
-#zval_spatial = colMeans(mvrnorm(n=10,yobs,sigma))
+# gaussian mean
+range = 1
+phi = 3/range
 
-#zval_independent = colMeans(mvrnorm(n=10,yobs,diag(n)))
+sigma = exp(-phi*dist)
 
+zval = mvrnorm(n=2,yobs,sigma)
+gp_mean = colMeans(zval)
+gp_median = apply(zval,2,median)
 
-plotdata = data.frame(yobs,znew, s, id = 1:n)
-datalong = reshape(plotdata, varying = colnames(plotdata)[c(1:2)], direction = "long", v.names = 'obs', times = c('original observation','spatial information through gp'))
+# make it correlated
+range = 0.05
+phi = 3/range
+sigma = exp(-phi*dist)
+eig = eigen(sigma)
+
+L = eig$vectors%*%diag(sqrt(eig$values))%*%t(eig$vectors)
+
+ycorr = L%*%yobs
+
+# plotting the results
+
+plotdata = data.frame(s, id = 1:n,yobs,gp_mean,weight,ycorr)
+datalong = reshape(plotdata, varying = colnames(plotdata)[-c(1:3)], direction = "long", v.names = 'obs', times = colnames(plotdata)[-c(1:3)])
 
 ggplot(datalong, aes(x = sx, y = sy, col = obs))+
-  geom_point(cex = 2)+
+  geom_point(cex = 4, alpha = 0.5)+
   facet_grid(cols = vars(time))
 
-length(znew)
-dim(s)
+
+
+
+
 Tdata = 1000
 #cdf_mu = rbeta(1,1,2)
 
