020eff0ccb0c6b5d62e63faa09d950598ad302e3
Ragnhild Laursen <ragnhild@math.au.dk>
2023-11-10 09:15:43 -0800

    small updates

--- GaussianPrior/runNMF.R
+++ GaussianPrior/runNMF.R
@@ -2,18 +2,22 @@ library(Rcpp)
 library(RcppArmadillo)
 sourceCpp("model/NMFspatial2.cpp")
 
-#path = "/gladstone/engelhardt/pelka-collaboration/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427"
-#setwd(path)
+path1 = '/gladstone/engelhardt/pelka-collaboration/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/cellpose_cell_by_gene_goodgenes_noblanks.csv'
+count = read.csv(path1, row.names = 1)
+count = as.matrix(count)
 
-#data = read.csv('cellpose_cell_by_gene_goodgenes_noblanks.csv', row.names = 1)
-#data = as.matrix(data)
+
 
 # load the data count
-data = read.csv('data/LH_counts.csv', header = T, row.names = 1)
-data = as.matrix(data)
+#data = read.csv('data/LH_counts.csv', header = T, row.names = 1)
+#data = as.matrix(data)
+idx = rowSums(count > 0) >= 20
 
 
-out = nmfgen(data, 10, iter = 5000)
-save(out, file = paste0("modelssaved/LH_f10_regnmf.RData"))
+count = count[idx,]
+
+out = nmfgen(count, 25, iter = 3000)
+
+save(out, file = paste0("modelssaved/cellpose_f25_nmfgen_over20genes.RData"))
 
 
--- GaussianPrior/runNMFspatial.R
+++ GaussianPrior/runNMFspatial.R
@@ -15,14 +15,16 @@ X2 = matrix(X2, nrow = length(X2), ncol = length(X2), byrow = F)
 dist = sqrt(X2 - 2*X%*%t(X) + t(X2))
 
 
-sigma = exp(-0.005*dist)
-sigma[sigma < 0.5] = 0
+sigma = exp(-0.01*dist)
+sigma[sigma < 0.2] = 0
 sigma = sigma/rowSums(sigma)
 
 
+for(i in 50:100){
+    out = nmfspatial(count, i,weight = sigma, initial = 1, smallIter = 100, maxiter = 5000)
+    save(out, file = paste0("modelssaved/LH5K_f",i,"_l001.RData"))
+}
 
-out = nmfspatial(count, 10,weight = sigma, initial = 1, smallIter = 100, maxiter = 5000)
-save(out, file = paste0("modelssaved/LH5K_f10_l0001.RData"))
 
 
 
