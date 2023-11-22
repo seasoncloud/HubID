da2a9a660c50377134e78c205abdc4ed16ef30bc
Ragnhild Laursen <ragnhild@math.au.dk>
2023-11-22 15:34:18 -0800

    correlation and multi sample

---
+++ GaussianPrior/runNMFspatial_multisample.R
@@ -0,0 +1,74 @@
+
+source("model/NMFbatch.R")
+
+# load dataset 1
+path1 = '/gladstone/engelhardt/pelka-collaboration/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/cellpose_cell_by_gene_goodgenes_noblanks.csv'
+count = read.csv(path1, row.names = 1)
+count1 = as.matrix(count)
+path2 = '/gladstone/engelhardt/pelka-collaboration/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/HuColonCa-FFPE-ImmuOnco-LH_VMSC02001_20220427/cellpose_cell_metadata.csv'
+metadata = read.csv(path2, row.names = 1)
+
+location = metadata[,c('center_x','center_y')]
+location1 = as.matrix(location)
+
+idx = rowSums(count1) >= 20
+
+location1 = location1[idx,]
+count1 = count1[idx,]
+
+batch_id1 = groupondist(location1,size = 5000)
+batch_id1 = paste0("1.",batch_id1)
+
+# load dataset 2
+path1 = '/gladstone/engelhardt/pelka-collaboration/HumanColonCancerPatient1/cell_by_gene.csv'
+count = read.csv(path1, row.names = 1)
+count2 = as.matrix(count)
+path2 = '/gladstone/engelhardt/pelka-collaboration/HumanColonCancerPatient1/cell_metadata.csv'
+metadata = read.csv(path2, row.names = 1)
+
+location = metadata[,c('center_x','center_y')]
+location2 = as.matrix(location)
+
+idx = rowSums(count2) >= 20
+
+good_genes = (colnames(count2) %in% colnames(count1))
+
+location2 = location2[idx,]
+count2 = count2[idx,good_genes]
+
+batch_id2 = groupondist(location2,size = 5000)
+batch_id2 = paste0("2.",batch_id2)
+
+# load dataset 3
+path1 = '/gladstone/engelhardt/pelka-collaboration/HumanColonCancerPatient2/cell_by_gene.csv'
+count = read.csv(path1, row.names = 1)
+count3 = as.matrix(count)
+path2 = '/gladstone/engelhardt/pelka-collaboration/HumanColonCancerPatient2/cell_metadata.csv'
+metadata = read.csv(path2, row.names = 1)
+
+location = metadata[,c('center_x','center_y')]
+location3 = as.matrix(location)
+
+idx = rowSums(count3) >= 20
+
+good_genes = (colnames(count3) %in% colnames(count1))
+
+location3 = location3[idx,]
+count3 = count3[idx,good_genes]
+
+batch_id3 = groupondist(location3,size = 5000)
+batch_id3 = paste0("3.",batch_id3)
+
+total_batch = c(batch_id1,batch_id2,batch_id3)
+total_count = rbind(count1,count2,count3)
+total_location = rbind(location1,location2,location3)
+
+row_sum = rowSums(total_count)
+total_count = total_count/row_sum*max(row_sum)
+
+start = Sys.time()
+out = nmfspatial_batch(total_count, 20, location = total_location, batch = total_batch, lengthscale = 500, initial = 1, smallIter = 10, maxiter = 2000, tolerance = 1e-10, error_freq = 100)
+stop = Sys.time()
+out$time = stop - start
+print(stop - start)
+save(out, file = paste0("modelssaved/three_sample_f20_s5K_l500_norm.RData"))
--- GaussianPrior/runNMFspatialbatch.R
+++ GaussianPrior/runNMFspatialbatch.R
@@ -17,16 +17,24 @@ metadata = read.csv(path2, row.names = 1)
 location = metadata[,c('center_x','center_y')]
 location = as.matrix(location)
 
-
-
-idx = rowSums(count) > 0
+idx = rowSums(count) > 50
 
 location = location[idx,]
 count = count[idx,]
 
-n = nrow(location)
+batch_id = groupondist(location,size = 5000)
 
-batch_id = groupondist(location,150)
+start = Sys.time()
+out = nmfspatial_batch(count, 20, location = location, batch = batch_id, lengthscale = 500, initial = 1, smallIter = 1, maxiter = 50, tolerance = 1e-10, error_freq = 10)
+stop = Sys.time()
+out$time = stop - start
+print(stop - start)
 
-out = nmfspatial_batch(count, 20, location = location, batch = batch_id, lengthscale = 300, initial = 1, smallIter = 500, maxiter = 3000, tolerance = 1e-10, freq_error = 100)
-save(out, file = paste0("modelssaved/cellpose_f20_b150_l300_1e10.RData"))
+start = Sys.time()
+out = nmfspatial_batch2(count, 20, location = location, batch = batch_id, lengthscale = 500, initial = 1, smallIter = 1, maxiter = 50, tolerance = 1e-10, error_freq = 10)
+stop = Sys.time()
+out$time = stop - start
+print(stop - start)
+
+
+#save(out, file = paste0("modelssaved/cellpose_f20_s5K_l500.RData"))
